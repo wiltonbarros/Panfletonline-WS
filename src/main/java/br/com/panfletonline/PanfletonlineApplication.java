eb6c4b375ee6fccd09e0e7fbcac103c7c1b9216a
Wilton <wilton.barros@hotmail.com>
2022-06-19 15:35:15 -0300

    commit inicial

---
+++ src/main/java/br/com/panfletonline/PanfletonlineApplication.java
@@ -0,0 +1,13 @@
+package br.com.panfletonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanfletonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanfletonlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/panfletonline/PanfletonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.panfletonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanfletonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

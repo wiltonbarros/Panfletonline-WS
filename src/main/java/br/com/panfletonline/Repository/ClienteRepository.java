2c8fd70d67649f58dcd478e3f974a1f44829face
wiltonbarros <wilton.barros@hotmail.com>
2022-06-21 16:14:54 -0300

    Alteracoes classes

--- src/main/java/br/com/panfletonline/Repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.panfletonline.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.panfletonline.Entity.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-
-}
